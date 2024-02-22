6e7dede98f294451a0c3cb9f451c4c336886c6c5
Flacko <lesliejoe2k2@gmail.com>
2024-02-22 05:25:11 +0000

    added finishing touches

---
+++ script.js
@@ -0,0 +1,101 @@
+// Variables 
+
+let btn = document.querySelector("#new-quote");
+let quote = document.querySelector(".quote");
+let person = document.querySelector(".person");
+
+const quotes = [
+    {
+        quote: "The way to get started is to quit talking and begin doing.",
+        author: "Walt Disney"
+    },
+    {
+        quote: "Life is what happens when you're busy making other plans.",
+        author: "John Lennon"
+    },
+    {
+        quote: "You miss 100% of the shots you don't take.",
+        author: "Wayne Gretzky"
+    },
+    {
+        quote: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt"
+    },
+    {
+        quote: "It's not whether you get knocked down, it's whether you get up.",
+        author: "Vince Lombardi"
+    },
+    {
+        quote: "You must be the change you wish to see in the world.",
+        author: "Mahatma Gandhi"
+    },
+    {
+        quote: "In the end, it's not the years in your life that count. It's the life in your years.",
+        author: "Abraham Lincoln"
+    },
+    {
+        quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+        author: "Mother Teresa"
+    },
+    {
+        quote: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+        author: "Robert Louis Stevenson"
+    },
+    {
+        quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+        author: "Franklin D. Roosevelt"
+    },
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela"
+    },
+    {
+        quote: "Life is either a daring adventure or nothing at all.",
+        author: "Helen Keller"
+    },
+    {
+        quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+        author: "Thomas A. Edison"
+    },
+    {
+        quote: "You can't go back and change the beginning, but you can start where you are and change the ending.",
+        author: "C.S. Lewis"
+    },
+    {
+        quote: "It does not matter how slowly you go as long as you do not stop.",
+        author: "Confucius"
+    },
+    {
+        quote: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs"
+    },
+    {
+        quote: "The best revenge is massive success.",
+        author: "Frank Sinatra"
+    },
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela"
+    },
+    {
+        quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+        author: "Albert Einstein"
+    },
+    {
+        quote: "In three words I can sum up everything I've learned about life: it goes on.",
+        author: "Robert Frost"
+    }
+];
+ 
+const showNextQuote = () => {
+    // equation to generate a random integer index from the quotes array and rorund it down to the nearest whole number
+
+    let random = Math.floor(Math.random() * quotes.length);
+    console.log(random)
+
+    quote.innerText = quotes[random].quote;
+    person.innerText = quotes[random].author;
+
+}
+
+btn.addEventListener("click", showNextQuote)
